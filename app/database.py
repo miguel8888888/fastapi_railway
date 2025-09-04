d8cade83e25a933f29494af9da699397b0a57ffc
miguel8888888 <86738300+miguel8888888@users.noreply.github.com>
2025-09-04 09:56:10 -0500

    conectar con railway

--- app/database.py
+++ app/database.py
@@ -5,13 +5,22 @@ import os
 
 # Variables de entorno
 # postgresql://postgres:edCDJOBJPvuRByUSMSkmOdbemJyDGRdh@turntable.proxy.rlwy.net:19716/railway
-DB_USER = os.getenv("DB_USER", "postgres")
-DB_PASS = os.getenv("DB_PASS", "edCDJOBJPvuRByUSMSkmOdbemJyDGRdh")
-DB_HOST = os.getenv("DB_HOST", "turntable.proxy.rlwy.net")
-DB_PORT = os.getenv("DB_PORT", "19716")
-DB_NAME = os.getenv("DB_NAME", "railway")
+# DB_USER = os.getenv("DB_USER", "postgres")
+# DB_PASS = os.getenv("DB_PASS", "edCDJOBJPvuRByUSMSkmOdbemJyDGRdh")
+# DB_HOST = os.getenv("DB_HOST", "turntable.proxy.rlwy.net")
+# DB_PORT = os.getenv("DB_PORT", "19716")
+# DB_NAME = os.getenv("DB_NAME", "railway")
 
-SQLALCHEMY_DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+# SQLALCHEMY_DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
+# engine = create_engine(SQLALCHEMY_DATABASE_URL)
+# SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# Railway ya expone la cadena completa en DATABASE_URL
+SQLALCHEMY_DATABASE_URL = os.getenv(
+    "DATABASE_URL",
+    "postgresql://postgres:edCDJOBJPvuRByUSMSkmOdbemJyDGRdh@turntable.proxy.rlwy.net:19716/railway"
+)
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
