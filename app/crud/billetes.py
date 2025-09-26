63aead75c293a8fcb8c17705f7d1088910908fb9
miguel8888888 <86738300+miguel8888888@users.noreply.github.com>
2025-09-25 21:23:58 -0500

    nuevos ajustes para render

--- app/crud/billetes.py
+++ app/crud/billetes.py
@@ -3,7 +3,7 @@ from app.models.billetes import Billete
 from app.schemas.billetes import BilleteCreate
 
 def create_billete(db: Session, billete: BilleteCreate):
-    db_billete = Billete(**billete.dict())
+    db_billete = Billete(**billete.model_dump())  # Cambia dict() por model_dump()
     db.add(db_billete)
     db.commit()
     db.refresh(db_billete)
@@ -13,7 +13,7 @@ def get_billetes(db: Session, skip: int = 0, limit: int = 100):
     return db.query(Billete).options(joinedload(Billete.pais_rel)).offset(skip).limit(limit).all()
 
 def get_billete(db: Session, billete_id: int):
-    return db.query(Billete).filter(Billete.id == billete_id).first()
+    return db.query(Billete).options(joinedload(Billete.pais_rel)).filter(Billete.id == billete_id).first()
 
 def update_billete(db: Session, billete_id: int, billete_data: BilleteCreate):
     billete = db.query(Billete).filter(Billete.id == billete_id).first()
@@ -21,6 +21,8 @@ def update_billete(db: Session, billete_id: int, billete_data: BilleteCreate):
         billete.anverso = billete_data.anverso
         billete.reverso = billete_data.reverso
         billete.pais = billete_data.pais
+        billete.denominacion = billete_data.denominacion  # Faltaba esta línea
+        billete.precio = billete_data.precio  # Faltaba esta línea
         db.commit()
         db.refresh(billete)
     return billete
